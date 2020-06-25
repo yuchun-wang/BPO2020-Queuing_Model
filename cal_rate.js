7f8149dbf6447b5d3142f9fe2ed77d7662fba7ad
yuchun <yuc1996229@gmail.com>
2020-06-25 18:11:07 +0800

    fix style

--- cal_rate.js
+++ cal_rate.js
@@ -6,10 +6,10 @@ sr2r.addEventListener('change', typeI_rate_Calculate);
 
 function typeII_rate_Calculate(e) {
   var y = 100 - parseFloat(e.target.value);
-  document.getElementById("_sr2r").innerHTML = 'Purchase Probability（%）- TypeII<input type="number" class="form-control SR2R" id="SR2R" max="100" min="0" value=' + y + '>';
+  document.getElementById("_sr2r").innerHTML = 'Purchase Probability (%) - TypeII<input type="number" class="form-control SR2R" id="SR2R" max="100" min="0" value=' + y + '>';
 }
 
 function typeI_rate_Calculate(e) {
   var y = 100 - parseFloat(e.target.value);
-  document.getElementById("_srr").innerHTML = 'Purchase Probability（%）- TypeI<input type="number" class="form-control SRR" id="SRR" max="100" min="0" value=' + y + '>';
+  document.getElementById("_srr").innerHTML = 'Purchase Probability (%) - TypeI<input type="number" class="form-control SRR" id="SRR" max="100" min="0" value=' + y + '>';
 }
